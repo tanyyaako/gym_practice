5fc52714de62cefaa7a394d34f9cb8ad511776dd
tanyyaako <tanyyaako@icloud.com>
2024-07-10 16:10:58 +0300

    Initial commit

---
+++ src/main/java/org/example/entities/CoachEntity.java
@@ -0,0 +1,9 @@
+package org.example.entities;
+
+public class coach {
+    private Long id;
+    private String name;
+    private String surname;
+    private String contactNumber;
+    private 
+}

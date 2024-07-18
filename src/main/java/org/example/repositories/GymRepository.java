4179781037850e26e4946edc4553c043924307c3
tanyyaako <tanyyaako@icloud.com>
2024-07-18 16:17:58 +0300

    fix

--- src/main/java/org/example/repositories/GymRepository.java
+++ src/main/java/org/example/repositories/GymRepository.java
@@ -1,7 +1,6 @@
 package org.example.repositories;
 
 import org.example.entities.GymEntity;
-import org.example.entities.MembershipCardEntity;
 
 public interface GymRepository extends GeneralRepository<GymEntity,Long>{
 
